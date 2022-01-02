f8556b73edc731132a2e89ff3b1574090afa72cf
russ430 <russ7166@gmail.com>
2022-01-02 11:12:11 -0800

    sync'd updates on homescreen to firebase

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -8,16 +8,32 @@ import RaceItem from '../components/RaceItem';
 import SectionHeader from '../components/SectionHeader';
 import { SafeAreaView } from '../components/Themed';
 import UpdateItem from '../components/UpdateItem';
-import { Race } from '../components/types';
+import { Race, Update } from '../components/types';
 
 export default function HomeScreen() {
   const [races, setRaces] = useState<Race[] | null>(null);
   const racesCollectionRef = collection(db, 'races');
+  const [updates, setUpdates] = useState<Update[] | null>(null);
+  const updatesCollectionRef = collection(db, 'updates');
 
   useEffect(() => {
+    const getupdates = async () => {
+      const updateDataRaw = await getDocs(updatesCollectionRef);
+      const updateData = updateDataRaw.docs.map((doc) => {
+        const { author, body, date, position } = doc.data();
+        return {
+          author,
+          body,
+          date,
+          position,
+          id: doc.id,
+        };
+      });
+      setUpdates(updateData);
+    };
     const getRaces = async () => {
-      const data = await getDocs(racesCollectionRef);
-      const raceData = data.docs.map((doc) => {
+      const raceDataRaw = await getDocs(racesCollectionRef);
+      const raceData = raceDataRaw.docs.map((doc) => {
         const { boathouse, end_date, location, race_name, start_date } =
           doc.data();
         return {
@@ -31,6 +47,7 @@ export default function HomeScreen() {
       });
       setRaces(raceData);
     };
+    getupdates();
     getRaces();
   }, []);
   return (
@@ -43,6 +60,11 @@ export default function HomeScreen() {
         <Text>Loading...</Text>
       )}
       <SectionHeader>Latest Updates</SectionHeader>
+      {updates
+        ? updates
+            .slice(0, 3)
+            .map((update) => <UpdateItem key={update.id} update={update} />)
+        : null}
     </SafeAreaView>
   );
 }
