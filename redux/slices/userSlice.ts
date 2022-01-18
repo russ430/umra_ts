c0bb76e35f55423f06d904635965d563cdf4780e
russ430 <russ7166@gmail.com>
2022-01-17 17:27:19 -0800

    removed auth error handling from reducer

--- redux/slices/userSlice.ts
+++ redux/slices/userSlice.ts
@@ -2,25 +2,18 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface UserData {
   email: string;
-  grad_year: string;
+  grad_year: number;
   name: string;
   position: string;
   admin: Boolean;
 }
 
-interface Error {
-  code: string;
-  name: string;
-}
-
 interface InitialState {
-  error: Error | null;
   isLoading: Boolean;
   data: UserData | null;
 }
 
 const initialState: InitialState = {
-  error: null,
   isLoading: false,
   data: null,
 };
@@ -32,11 +25,8 @@ const userSlice = createSlice({
     setUser(state, action: PayloadAction<UserData | null>) {
       state.data = action.payload;
     },
-    setError(state, action: PayloadAction<Error | null>) {
-      state.error = action.payload;
-    },
   },
 });
 
-export const { setError, setUser } = userSlice.actions;
+export const { setUser } = userSlice.actions;
 export default userSlice.reducer;
